f862adef73076affec52f7873db0b027c4bcdcb6
Icyscools <meet.ice1@gmail.com>
2019-11-15 04:39:51 +0700

    Fix wrong import json

--- js/planetsScene.js
+++ js/planetsScene.js
@@ -4,21 +4,21 @@ var parallaxInstance = new Parallax(scene);
 // Lazy load
 var lazyLoadInstance = new LazyLoad({
   elements_selector: ".lazy",
-  callback_reveal: function (el) {
+  callback_reveal: function(el) {
     // console.log("Start loading...")
     // console.log(el)
-    setTimeout(function () {
+    setTimeout(function() {
       if (loadPlanet === false) {
-        showLoadingOverlay()
+        showLoadingOverlay();
       }
-    }, 150)
+    }, 150);
   },
-  callback_loaded: function () {
+  callback_loaded: function() {
     // console.log("Finish loading :)")
-    let content = document.getElementById("content")
+    let content = document.getElementById("content");
     content.style.opacity = "1";
     loadPlanet = true;
-    hideLoadingOverlay()
+    hideLoadingOverlay();
   }
 });
 
@@ -39,7 +39,7 @@ function changePlanet(el) {
   let tags = document.getElementsByClassName("card-body");
   if (el != activePlanet || activePlanet == null) {
     content.style.opacity = "0";
-    setTimeout(function () {
+    setTimeout(function() {
       let facts = dataPlanet[planet].fact;
       [...tags].forEach((element, index) => {
         element.getElementsByClassName(
@@ -71,18 +71,6 @@ function changePlanet(el) {
   }
 }
 
-function getJSON(url, callback) {
-  let req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState == 4 && req.status == 200) {
-      let json = JSON.parse(req.responseText);
-      callback(json);
-    }
-  };
-  req.open("GET", url, true);
-  req.send();
-}
-
 function hideLoadingOverlay() {
   let overlay = document.getElementById("overlay");
   overlay.style.opacity = "0";
@@ -95,8 +83,20 @@ function showLoadingOverlay() {
   overlay.style.zIndex = "1000";
 }
 
-window.onload = function () {
-  getJSON("/js/planets.json", function (res) {
+function getJSON(url, callback) {
+  let req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+      let json = JSON.parse(req.responseText);
+      callback(json);
+    }
+  };
+  req.open("GET", url, true);
+  req.send();
+}
+
+window.onload = function() {
+  getJSON("./js/planets.json", function(res) {
     dataPlanet = res;
     let tags = document.getElementById("nav-planet").getElementsByTagName("a");
     [...tags].forEach(element => {
@@ -105,18 +105,16 @@ window.onload = function () {
 
     let hash = window.location.hash.substr(1);
     if (hash && hash.trim() != "" && dataPlanet.hasOwnProperty(hash)) {
-      let target = [...tags].filter(el => el.getAttribute("data-planet") == hash)[0];
+      let target = [...tags].filter(
+        el => el.getAttribute("data-planet") == hash
+      )[0];
       if (target != null) {
         this.changePlanet(target);
       } else {
-        this.changePlanet(
-          [...tags][0]
-        );
+        this.changePlanet([...tags][0]);
       }
     } else {
-      this.changePlanet(
-        [...tags][0]
-      );
+      this.changePlanet([...tags][0]);
     }
   });
 };
